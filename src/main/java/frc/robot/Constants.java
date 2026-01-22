8837df2531dfd4e21735d1ef03042012b8e4d533
Nfoodd <140904904+Nfoodd@users.noreply.github.com>
2026-01-22 18:16:03 -0500

    We created a new Hang Subsystem class and defined the Hang motor. It reuses the motor setting method from the wall subsystem currently.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,9 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class HangConstants {
+    public static final double kHangGroundRotations = 0;
+    public static final double kHangL1Rotations = 0;
+    public static final double kHangLevelTolerance = 0;
+  }
 }
