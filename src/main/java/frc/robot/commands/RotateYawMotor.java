991baa4ccbe1509e20862db54a7170824eda4da0
Micah-Lander <lander.22@daytonstemschool.org>
2026-02-04 17:23:47 -0500

    added a yaw motor for the shooter, and an accompanying command.

---
+++ src/main/java/frc/robot/commands/RotateYawMotor.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RotateYawMotor extends Command {
+
+  private final ShooterSubsystem m_subsystem;
+
+  public RotateYawMotor(ShooterSubsystem shooter) {
+    m_subsystem = shooter;
+    addRequirements(shooter);
+  }
+
+
+  @Override
+  public void initialize() {
+    m_subsystem.rotateYawMotor(
+      Degrees.of(25)
+    );
+  }
+
+
+  @Override
+  public void execute() {}
+
+
+  @Override
+  public void end(boolean interrupted) {
+    m_subsystem.rotateYawMotor(
+      Degrees.of(0));
+  }
+
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.DegreesPerSecond;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -15,13 +16,16 @@ import com.ctre.phoenix6.hardware.TalonFX;
 public class ShooterSubsystem extends SubsystemBase {
   private TalonFX m_launchMotor;
   private TalonFX m_pitchMotor;
+  private TalonFX m_yawMotor;
   private Slot0Configs launchMotorConfigs;
   private Slot0Configs pitchMotorConfigs;
+  private Slot0Configs yawMotorConfigs;
   private VelocityVoltage launchRequest;
   private PositionVoltage pitchRequest;
+  private PositionVoltage yawRequest;
   private Angle targetPosition;
 
-  public ShooterSubsystem(int launchMotorId, int pitchMotorId) {
+  public ShooterSubsystem(int launchMotorId, int pitchMotorId, int yawMotorId) {
     
     m_launchMotor = new TalonFX(launchMotorId);
     launchMotorConfigs = new Slot0Configs();
@@ -32,8 +36,30 @@ public class ShooterSubsystem extends SubsystemBase {
     launchMotorConfigs.kD = 0;
     m_launchMotor.getConfigurator().apply(launchMotorConfigs);
     launchRequest = new VelocityVoltage(0).withSlot(0);
-    
+
+    m_yawMotor = new TalonFX(yawMotorId);
+    yawMotorConfigs = new Slot0Configs();
+    //Placeholder PID values
+    yawMotorConfigs.kV = 0;
+    yawMotorConfigs.kP = 1;
+    yawMotorConfigs.kI = 0;
+    yawMotorConfigs.kD = 0;
+    m_yawMotor.getConfigurator().apply(yawMotorConfigs);
+    yawRequest = new PositionVoltage(0).withSlot(0);
+  
   }
+   
+  //rotateYawMotor is a PositionVoltage
+  public void rotateYawMotor(double speed) {
+    rotateYawMotor(Degrees.of(speed));
+}
+
+  public void rotateYawMotor(Angle speed) {
+    m_yawMotor.setControl(yawRequest.withPosition(speed));
+  }
+
+    
+  
 
    
 
