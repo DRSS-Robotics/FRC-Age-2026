ead1a38a5b111b10c5775c708a8cf80b878424ec
Lewisv2 <Wklewis5@gmail.com>
2026-03-07 15:08:27 -0500

    Added code about getting data from the NavGrid.json

--- src/main/java/frc/robot/subsystems/NavGridCoord.java
+++ src/main/java/frc/robot/subsystems/NavGridCoord.java
@@ -15,31 +15,54 @@ import javax.xml.crypto.Data;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-//import javax.json.JsonBuilderFactory;
+import org.json.JSONTokener;
 import frc.robot.subsystems.NavGrid;
-import org.json.JSONArray;
-import org.json.Tokener;
+
 
 public class NavGridCoord {
 
   public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
+        NavGrid stgrid = new NavGrid();
         String filePath = "src/main/deploy/pathplanner/navgrid.json";
 
         // File file = Paths.get(filePath).toFile();
         //     System.out.println(file);
 
         // boolean[][] plane = new boolean[(int) 16.54][(int) 8.07];
-      try {
-            FileReader reader = new FileReader(filePath);
-            JSONTokener Tokener = new JSONTokener(reader);
+      try(FileReader reader = new FileReader(filePath)) {
+            // FileReader reader = new FileReader(filePath);
+            // JSONTokener tokener = new JSONTokener(reader);
+            // JSONObject jsonObject = new JSONObject();
+            StringBuilder StringB = new StringBuilder();
+            int character;
+            while ((character = reader.read()) != -1) {
+                StringB.append((char) character);
+            }
+            String jsonContent = StringB.toString();
+
+            JSONObject jsonObject = new JSONObject(jsonContent);
+
+            //Get the "activeStates" array as a JSONArray
+            JSONArray jsonArray = jsonObject.getJSONArray("grid");
+
+            //Convert the JSONArray to a Java boolean array or List<Boolean>
+            boolean[] booleanArray = new boolean[jsonArray.length()];
+            for(int i = 0; i < jsonArray.length(); i++) {
+                booleanArray[i] = jsonArray.getBoolean(i);
+            }
+        
+            //Print the resulting boolean array
+            System.out.println("Boolean Array: ");
+            for (boolean b : booleanArray) {
+                System.out.println(b);
+            }
     
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-
 
 
 //    public static void main(String[] args) {
