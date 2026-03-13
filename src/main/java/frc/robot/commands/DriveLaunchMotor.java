be5dab90f205257645e8ec3a691c0634f5a73a3b
Lewisv2 <Lewisv.2@dayonstemschool.org>
2026-03-13 16:56:51 -0400

    did more full-robot testing

--- src/main/java/frc/robot/commands/DriveLaunchMotor.java
+++ src/main/java/frc/robot/commands/DriveLaunchMotor.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 import static edu.wpi.first.units.Units.DegreesPerSecond;
