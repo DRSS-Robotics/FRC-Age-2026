2b6a3353099ad4a5663acf5d0fff53de1f58eab3
Lewisv2 <Lewisv.2@dayonstemschool.org>
2026-03-31 16:26:57 -0400

    Nothing change commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -75,7 +75,7 @@ public class Robot extends TimedRobot {
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
-    // this line or comment it out.
+    // this line or comment it out
 
     //Didn't change this, don't think it's the problem -Micah plp
     if (m_autonomousCommand != null) {
